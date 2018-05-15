b6fcc0d1804ab1b8644c7f5b2bac5db9ca4e62b1
Alberto Arias <alberto@boodland.com>
2018-05-15 14:21:01 +0200

    Add entry point and tests initialize to dummy values

---
+++ src/server/index.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const should = chai.should();
+
+describe('Index Test', () => {
+  it('should pass', (done) => {
+    const sum = 1 + 2;
+    sum.should.eql(3);
+    sum.should.not.eql(4);
+    done();
+  });
+});
