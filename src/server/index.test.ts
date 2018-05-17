8132cb80b16a8210701f1c233bcd95bc2ba729bf
Alberto Arias <alberto@boodland.com>
2018-05-17 14:16:10 +0200

    Add Server suite test with Create Server App and Run Server App tests

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -1,12 +1,53 @@
-import { should } from 'chai'
+import { ServerApp } from './index';
 
-describe('Index Test', () => {
-  it('should pass', (done) => {
-    const sum = 1 + 2;
-    should().equal(sum, 3);
-    should().not.equal(sum, 4);
-    done();
-  });
+import 'mocha'
+import { should, expect, use, request } from 'chai';
+
+process.env.NODE_ENV = 'test';
+const port = 3001
+const serverApp = new ServerApp(3001);
+
+after(function(done) {
+  done();
 });
+
+describe('Server', function() {
+
+  describe('Create Server App', function() {
+    
+    it('should exists', function() {
+      should().exist(serverApp);
+    });
+
+    it('expect to be ServerApp instance', function() {
+      expect(serverApp).to.be.an.instanceof(ServerApp)
+    });
+
+  });
+
+  describe('Run server app', function() {
+
+    use(require('chai-http'));
+    const webServer = serverApp.run();
+    
+    it('should run on localhost:3001', function(done) {
+      request('http://localhost:3001')
+      .get('/')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(404);
+        done()
+      })
+    });
+
+    after(function() {
+      webServer.close();
+    })
+
+  });
+
+});
+
+
 
 
