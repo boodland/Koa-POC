322a44093f331bdda282ffe0c95b54ac83918c9c
Alberto Arias <alberto@boodland.com>
2018-05-29 23:02:27 +0200

    Create Middleware with handler test and implementation

--- src/middleware/Middleware.test.ts
+++ src/middleware/Middleware.test.ts
@@ -1,19 +1,45 @@
 import { should, expect, } from 'chai';
 
-import Middleware from './Middleware';
+import Middleware, { MiddlewareHandler } from './Middleware';
+import { Context, BaseContext } from 'koa';
 
 after(function(done) {
     done();
 });
 
 describe('Middleware', function() {
-
-    const middleware = new Middleware();
+    
+    const message = "Hello World!"
+    const fooHandler: MiddlewareHandler = (options=message) => {
+        
+        const configuration = options;
+        
+        return async function Foo(mock, next) {
+            mock.state = {
+                message: configuration,
+            };
+            await next();
+        };
+    }
+    const middleware = new Middleware(fooHandler);
 
     describe('Create Middleware', function() {
         
         it('should exists', function() {
             should().exist(middleware);
+        });
+
+        
+        it(`should set message to ${message}`, async function() {
+            
+            let mock = <Context>{};
+            const noop = (): Promise<any> => { return };
+            
+            const handler = middleware.getFunction();
+            expect(handler).to.be.a('function');
+            await handler(mock, noop)
+            should().equal(mock.state.message, message);
+
         });
 
     });
--- src/middleware/Middleware.ts
+++ src/middleware/Middleware.ts
@@ -1,3 +1,19 @@
+import { Middleware as MiddlewareFunction } from 'koa'
+
+type MiddlewareHandler = (options?: any) => MiddlewareFunction;
+
 export default class Middleware {
 
+    private name: string;
+    private handler: MiddlewareHandler;
+
+    constructor (handler: MiddlewareHandler) {
+        this.handler = handler;
+    };
+
+    getFunction(options?: any) : MiddlewareFunction {
+        return this.handler(options);
+    };
 };
+
+export { MiddlewareHandler };
