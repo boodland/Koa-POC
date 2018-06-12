6f25b85841382876a2eb0de52644f7a1bf461df6
Alberto Arias <alberto@boodland.com>
2018-06-12 19:12:59 +0200

    Use middlewares in main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import ServerApp  from './server/index'
+import Middleware from './middleware/Middleware';
+import { authenticated, piPower, reverse } from './middleware/Handlers';
 
 const serverApp : ServerApp = new ServerApp();
-const server = serverApp.run();
+let middlewares: Middleware[] = [];
+middlewares.push(new Middleware(authenticated))
+middlewares.push(new Middleware(reverse))
+middlewares.push(new Middleware(piPower))
+serverApp.use(...middlewares);
+serverApp.run();
