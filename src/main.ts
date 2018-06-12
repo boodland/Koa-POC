9de4d7b8857023334a51e374a1bf4967b5b1a1f9
Alberto Arias <alberto@boodland.com>
2018-06-12 19:23:39 +0200

    Use middleware index to import all the dependencies Add reverse middleware with options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import ServerApp  from './server/index'
-import Middleware from './middleware/Middleware';
-import { authenticated, piPower, reverse } from './middleware/Handlers';
+import Middleware, { authenticated, piPower, reverse } from './middleware/index';
 
 const serverApp : ServerApp = new ServerApp();
 let middlewares: Middleware[] = [];
 middlewares.push(new Middleware(authenticated))
 middlewares.push(new Middleware(reverse))
+middlewares.push(new Middleware(reverse, 'forward'))
 middlewares.push(new Middleware(piPower))
 serverApp.use(...middlewares);
 serverApp.run();
