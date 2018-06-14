2735d2131c3d4a40eb07818ab9c62e0e576ee164
Alberto Arias <alberto@boodland.com>
2018-06-14 19:48:29 +0200

    Use routes in main, move response middleware handler to the bottom

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import { AdminRouter } from './router/admin';
 import ServerApp  from './server/index'
 import { MiddlewareHandler, authenticated, piPower, reverse } from './middleware/index';
 
 const serverApp : ServerApp = new ServerApp();
+
 let middlewares: MiddlewareHandler[] = [];
 middlewares.push(authenticated())
 middlewares.push(reverse())
 middlewares.push(reverse('forward'))
-middlewares.push(piPower())
 serverApp.use(...middlewares);
+
+const adminRouter = new AdminRouter();
+serverApp.routes(adminRouter);
+
+serverApp.use(piPower())
 serverApp.run();
